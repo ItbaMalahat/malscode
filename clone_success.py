ea8afaf12b47d19ff40f12adcece6581feea0eef
Itba Malahat <itbamalahat@gmail.com>
2025-10-22 12:54:59 +0500

    Initial commit

---
+++ clone_success.py
@@ -0,0 +1,20 @@
+# clone_success.py
+
+def celebrate():
+    print("\n🎉 Clone successful, woohoo! 🎉\n")
+    print("Let's party! 💃🕺\n")
+    party_art = r"""
+        ✨✨✨✨✨✨✨✨✨✨
+        🎈     🥳 PARTY TIME 🥳     🎈
+        ✨✨✨✨✨✨✨✨✨✨
+             (•_•)
+             <)   )╯  💃
+              /   \
+          (•_•)
+          (   (>   🕺
+          /   \
+    """
+    print(party_art)
+
+if __name__ == "__main__":
+    celebrate()
